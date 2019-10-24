ff6cb54588bcf391eebb438a3ec56a5aa465218b
PACTERA-242 <PACTERA-242@crc.com.hk>
2019-10-24 17:02:02 +0800

    添加springcloud bus 动态刷新配置

--- config-client/src/main/java/org/timer/ConfigClientApplication.java
+++ config-client/src/main/java/org/timer/ConfigClientApplication.java
@@ -3,8 +3,14 @@ package org.timer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@RefreshScope
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
